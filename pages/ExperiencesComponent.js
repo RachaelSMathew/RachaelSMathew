6529bc5645b3ddf8c712d996226518a4ead48f51
Rachael Mathew <rachaelmathew1@gmail.com>
2022-12-26 02:15:44 -0500

    Update ExperiencesComponent.js

--- pages/ExperiencesComponent.js
+++ pages/ExperiencesComponent.js
@@ -69,14 +69,14 @@ const Experiences = () => {
                 
                 
                 <Box sx={{ flexGrow: 1 }}>
-                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12, lg: 16}}>
-                <Grid item xs={0} sm={3} md={3} lg={3}>
+                <Grid container wrap="nowrap" spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12, lg: 16}}>
+                <Grid item zeroMinWidth xs={0} sm={3} md={3} lg={3}>
                 <ThemeProvider theme={theme}>
                 <Typography variant="h3" sx={{ fontWeight: 'bold' }}><div className="changeTextCompany"></div>
                 </Typography>
                 </ThemeProvider>
                 </Grid>
-                <Grid item xs={4} sm={5} md={9} lg={13}>
+                <Grid item zeroMinWidth xs={4} sm={5} md={9} lg={13}>
                 <ThemeProvider theme={theme}>
                 <Typography variant="h4" align="left" sx={{ fontWeight: 'bold' }}><div className="changeTextExperiences"></div>
                 </Typography>
