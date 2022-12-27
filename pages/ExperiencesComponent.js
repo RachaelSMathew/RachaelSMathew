c09edce2ad78b15470c6eb238597d61a847e7958
Rachael Mathew <rachaelmathew1@gmail.com>
2022-12-27 04:02:56 -0500

    Update ExperiencesComponent.js

--- pages/ExperiencesComponent.js
+++ pages/ExperiencesComponent.js
@@ -59,16 +59,7 @@ const Experiences = () => {
       />
                 
                 <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{delay:1.5,duration:1.5}}>
-               
-
-                <div className="separator">
-                <div className="line"></div>
-                <h1 className="ExperiencesTitleStyle">Experiences</h1>
-                <div className="line"></div>
-                </div>
-                
-               
-                <Tabs/>
+            	<Tabs/>
             
                 </motion.div>
                 </React.Fragment>
