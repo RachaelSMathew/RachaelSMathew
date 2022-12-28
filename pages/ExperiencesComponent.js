5effec127902a9e2c70d4f8e48406e5fc37e2adf
Rachael Mathew <rachaelmathew1@gmail.com>
2022-12-28 06:05:05 -0500

    Update ExperiencesComponent.js

--- pages/ExperiencesComponent.js
+++ pages/ExperiencesComponent.js
@@ -29,12 +29,12 @@ const Experiences = () => {
                 
                 <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{delay:1.5,duration:1.5}}>
                 <div className="separator"></div>
-	        <Grid className="containerExperiences" container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12, lg: 16}}>
-                    <Grid item xs={4} sm={4} md={3} lg={4}>
+	        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12, lg: 16}}>
+                    <Grid className="containerExperiences" item xs={4} sm={4} md={3} lg={4}>
                         <Image className="imageExperiences" src={NasaNuance}  width={200} height={200}/>
                             <div class="textExperiences">Nuance</div>
                     </Grid>
-                    <Grid item xs={4} sm={4} md={3} lg={4}>
+                    <Grid className="containerExperiences" item xs={4} sm={4} md={3} lg={4}>
                         <Image className="imageExperiences" src={PlutoCYE}  width={200} height={200}/>
                             <div class="textExperiences">CYE</div>
                     </Grid>
