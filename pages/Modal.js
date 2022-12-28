1d496612197b29fc83b3d6063fbaf1d04753490d
Rachael Mathew <rachaelmathew1@gmail.com>
2022-12-28 06:19:55 -0500

    Update Modal.js

--- pages/Modal.js
+++ pages/Modal.js
@@ -3,29 +3,21 @@ import React from 'react';
 const Modal = ({ open, onClose }) => {
   if (!open) return null;
   return (
-    <div onClick={onClose} className='overlay'>
+    <div onClick={onClose} className='overlayExp'>
       <div
         onClick={(e) => {
           e.stopPropagation();
         }}
-        className='modalContainer'
+        className='modalContainerExp'
       >
-        <div className='modalRight'>
-          <p className='closeBtn' onClick={onClose}>
+        <div className='modalRightExp'>
+          <p className='closeBtnExp' onClick={onClose}>
             X
           </p>
-          <div className='content'>
+          <div className='contentExp'>
             <p>Do you want a</p>
             <h1>$20 CREDIT</h1>
             <p>for your first tade?</p>
-          </div>
-          <div className='btnContainer'>
-            <button className='btnPrimary'>
-              <span className='bold'>YES</span>, I love NFT's
-            </button>
-            <button className='btnOutline'>
-              <span className='bold'>NO</span>, thanks
-            </button>
           </div>
         </div>
       </div>
