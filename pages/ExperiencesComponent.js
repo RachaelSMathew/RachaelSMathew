74be461f7804fe2addaf62a3e636349f50188cb1
Jenkins C.I <svc_jenkins@irobot.com>
2022-12-27 22:55:39 -0500

    changes

--- pages/ExperiencesComponent.js
+++ pages/ExperiencesComponent.js
@@ -6,13 +6,11 @@
 //
 import React, { useRef, Suspense } from 'react'
 import Script from 'next/script'
-import Tabs from './Tabs'
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom'
 import { useState, useEffect } from "react";
 //using curly brackets, within react-router-dom multiple modules you can pull from, no defualt, need to be specific
 import { experimentalStyled as styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import {motion} from 'framer-motion'
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
@@ -20,59 +18,48 @@ import { Typography, ThemeProvider } from '@mui/material';
 
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { TextureLoader } from 'three/src/loaders/TextureLoader.js'
-
+import Image from 'next/image'
+import PlutoCYE from '../images/PlutoCYE.png';
+import JupiteriRobot from '../images/JupiteriRobot.png';
+import NebulaUMass from '../images/NebulaUMass.png';
+import NasaNuance from '../images/NasaNuance.png';
+import Modal from './Modal';
 const Experiences = () => {
 
     let theme = createTheme();
     theme = responsiveFontSizes(theme);
-    
+    const [openModal, setOpenModal] = useState(false);
     
         return (
                 
                 <React.Fragment>
-                <Script
-                  dangerouslySetInnerHTML={{
-                    __html: `var text = ["Created automated Jenkins pipelines using EC2 instances and executed tasks in a Docker container using an ECR image", "Worked with Azure DevOps, using YAML pipelines, to create Xcode simulator builds, run snapshot tests. Created Veracode Archive and uploaded the scan, created a streamlined YAML pipeline with multiple sub-tasks using parallel jobs and templates for clean, high-efficiency code.", "Created a calendar, which could have events created and added on, as a part of the CYE iPad app. Sending iOS logs to AWS Cloudwatch using AWS Soto Swift Package Manager in Xcode", "Graded students work, tutored students one-on-one, Attended weekly CS 121 lab sessions to help students"];
-                            var companyName = ["iRobot", "Nuance", "Center for Youth Engagement", "Undergraduate Course Assistant"];
-                            var counter = 0;
-                            var experiencesClass = document.getElementsByClassName("changeTextExperiences")[0];
-			    var companiesClass = document.getElementsByClassName("changeTextCompany")[0];
-                            
-                            setInterval(() => {
-                                    experiencesClass.classList.add('hideExperiences');
-                                    companiesClass.classList.add('hideExperiences');
-                                setTimeout(function (event) {
-                                    experiencesClass.innerHTML = text[counter];
-                                    experiencesClass.classList.remove('hideExperiences');
-				                    
-                                    companiesClass.innerHTML = companyName[counter];
-                                    companiesClass.classList.remove('hideExperiences');
-                                    
-                                    counter++;
-                                    if (counter >= text.length) {
-                                        counter = 0;
-                                    }
-                                }, 500);
-
-                            }, 5000);
-            `,
-        }}
-      />
                 
                 <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{delay:1.5,duration:1.5}}>
                 <div className="separator"></div>
-	            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12, lg: 16}}>
-            	<Grid item xs={4} sm={8} md={7} lg={12}><Tabs/></Grid>
-                <Grid item xs={4} sm={8} md={5} lg={4}>
-		      <Canvas>
-		      <ambientLight intensity={0.5} />
-		      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-		      <pointLight position={[-10, -10, -10]} />
-		      <Suspense fallback={null}>
-		      <Box />
-		      </Suspense>
-		      </Canvas>
-		</Grid>
+	            <Grid className="containerExperiences" container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12, lg: 16}}>
+                    <Grid item xs={4} sm={4} md={3} lg={4}>
+                        <Image className="imageExperiences" src={JupiteriRobot}  width={200} height={200} onClick={() => setOpenModal(true)} />
+                            <Modal open={openModal} onClose={() => setOpenModal(false)} />
+                            <div class="textExperiences">iRobot</div>
+                    </Grid>
+                    <Grid item xs={4} sm={4} md={3} lg={4}>
+                        <Image className="imageExperiences" src={NasaNuance}  width={200} height={200}/>
+                            <div class="textExperiences">Nuance</div>
+                    </Grid>
+                    <Grid item xs={4} sm={4} md={3} lg={4}>
+                        <Image className="imageExperiences" src={PlutoCYE}  width={200} height={200}/>
+                            <div class="textExperiences">CYE</div>
+                    </Grid>
+                    <Grid item xs={4} sm={4} md={3} lg={4}>
+                        <Canvas>
+                        <ambientLight intensity={0.5} />
+                        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                        <pointLight position={[-10, -10, -10]} />
+                        <Suspense fallback={null}>
+                        <Box />
+                        </Suspense>
+                        </Canvas>
+                    </Grid>
                 </Grid>
                 </motion.div>
                 </React.Fragment>
--- pages/SkillsComponent.js
+++ pages/SkillsComponent.js
@@ -36,7 +36,12 @@ const Skills = () => {
                 <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{delay:2.5,duration:1.5}}>
                 <div className="separator"></div>
                 <Box sx={{ flexGrow: 1 }} className="flexBehindFooter">
-                  <Grid container spacing={{ xs: 4, md: 3 }} columns={{ xs: 4, sm: 9, md: 12 }}>
+                <Grid container spacing={{ xs: 4, md: 3 }} columns={{ xs: 4, sm: 9, md: 12 }}>
+                <Grid item xs={2} sm={3} md={2}>
+                <motion.div whileHover={{scale:1.1}} transition={{type: 'spring', stiffness: 800}}>
+                <h1 className = "styleSkills" style={{color: 'mediumpurple'}}>Skills</h1>
+                </motion.div>
+                </Grid>
                 <Grid item xs={2} sm={3} md={2}>
                 <motion.div whileHover={{scale:1.1}} transition={{type: 'spring', stiffness: 800}}>
                 <Image src={Java}  width={100}
@@ -82,18 +87,6 @@ const Skills = () => {
                 <Grid item xs={2} sm={3} md={2}>
                 <motion.div whileHover={{scale:1.1}} transition={{type: 'spring', stiffness: 800}}>
                 <Image src={AWSS3}  width={100}
-                height={100}/>
-                </motion.div>
-                </Grid>
-                <Grid item xs={2} sm={3} md={2}>
-                <motion.div whileHover={{scale:1.1}} transition={{type: 'spring', stiffness: 800}}>
-                <Image src={Wordpress}  width={100}
-                height={100}/>
-                </motion.div>
-                </Grid>
-                <Grid item xs={2} sm={3} md={2}>
-                <motion.div whileHover={{scale:1.1}} transition={{type: 'spring', stiffness: 800}}>
-                <Image src={Mapbox}  width={100}
                 height={100}/>
                 </motion.div>
                 </Grid>
