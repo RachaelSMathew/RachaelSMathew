5e9598c85a7a23214087045345b575e0c0ac7b65
Rachael Mathew <rachaelmathew1@gmail.com>
2022-12-29 01:52:29 -0500

    Update CubeThreeFiber.js

--- pages/CubeThreeFiber.js
+++ pages/CubeThreeFiber.js
@@ -1,20 +1,33 @@
-import React, { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 
 function Box(props) {
-   const mesh = useRef();
-   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
-   return (
-      <mesh {...props} ref={mesh}>
-         <boxGeometry args={[3, 3, 3]} />
-         <meshStandardMaterial color={"orange"} />
-      </mesh>
-   );
+  // This reference will give us direct access to the mesh
+  const mesh = useRef()
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (mesh.current.rotation.x += delta))
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
 }
 export default function CubeThreeFiber() {
    return (
       <Canvas>
          <ambientLight />
+         <pointLight position={[10, 10, 10]} />
          <Box position={[0, 0, 0]} />
       </Canvas>
    );
