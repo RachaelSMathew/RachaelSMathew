305d634639b39142648530d82b713716c0cf93a3
Rachael Mathew <rachaelmathew1@gmail.com>
2022-12-30 20:41:18 -0500

    Update FooterComponent.js

--- pages/FooterComponent.js
+++ pages/FooterComponent.js
@@ -11,19 +11,19 @@ const Footer = () => (
   <div className="footer">
     <div className="footer-main">
     <ul className="footer-links">
-        <li className="mouse-out"><a href="https://www.instagram.com/rachaelsmathew/"><i className="fab fa-instagram"><span style={{ marginLeft: "10px" }}><ThemeProvider theme={theme}>
+        <li style="color:#551b8b" className="mouse-out"><a href="https://www.instagram.com/rachaelsmathew/"><i className="fab fa-instagram"><span style={{ marginLeft: "10px" }}><ThemeProvider theme={theme}>
                       <Typography variant="h5" align="center"><div className="footerFont"> instagram </div> </Typography>
                     </ThemeProvider></span></i></a></li>
                       
-        <li className="mouse-out"><a href="https://www.facebook.com/abby.sherman.14268/"><i className="fab fa-facebook"><span style={{ marginLeft: "10px" }}><ThemeProvider theme={theme}>
+        <li style="color:#551b8b" className="mouse-out"><a href="https://www.facebook.com/abby.sherman.14268/"><i className="fab fa-facebook"><span style={{ marginLeft: "10px" }}><ThemeProvider theme={theme}>
                       <Typography variant="h5" align="center"><div className="footerFont"> facebook </div> </Typography>
                     </ThemeProvider></span></i></a></li>
                       
-        <li className="mouse-out"><a href="https://www.linkedin.com/in/rachael-mathew-227a18138/"><i className="fab fa-linkedin"><span style={{ marginLeft: "10px" }}><ThemeProvider theme={theme}>
+        <li style="color:#551b8b" className="mouse-out"><a href="https://www.linkedin.com/in/rachael-mathew-227a18138/"><i className="fab fa-linkedin"><span style={{ marginLeft: "10px" }}><ThemeProvider theme={theme}>
                       <Typography variant="h5" align="center"><div className="footerFont"> linkedin </div> </Typography>
                     </ThemeProvider></span></i></a></li>
                       
-        <li className="mouse-out"><a href="mailto:rmathew@umass.edu"><i className="fas fa-envelope"><span style={{ marginLeft: "10px" }}><ThemeProvider theme={theme}>
+        <li style="color:#551b8b" className="mouse-out"><a href="mailto:rmathew@umass.edu"><i className="fas fa-envelope"><span style={{ marginLeft: "10px" }}><ThemeProvider theme={theme}>
                         <Typography variant="h5" align="center"><div className="footerFont"> email </div> </Typography>
                       </ThemeProvider></span></i></a></li>
     </ul>
